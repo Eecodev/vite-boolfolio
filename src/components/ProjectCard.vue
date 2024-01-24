f26659b742b0b3e9998f5b805f393955f67f3b51
Eecodev <mnasser.bbk@gmail.com>
2024-01-24 17:18:17 +0100

    truncate title and body added

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,15 +1,42 @@
 <template>
-    <div>
-        <router-link :to="{ name: 'single-project', params: {id: project.id} }" class="btn btn-primary">
-                {{ project.title }}
-        </router-link>
+    <div class="card">
+        <img :src="`${store.imgPath}${post.image}`" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ truncateTitle }}</h5>
+            <p class="card-text">{{ truncateBody }}</p>
+            <router-link :to="{ name: 'single-project', params: {id: project.id} }" class="btn btn-primary">
+                    {{ project.title }}
+            </router-link>
+        </div>
     </div>
 </template>
 
 <script>
+import {store} from "../data/store";
     export default {
         name: 'ProjectCard',
-        // props: ['project']
+        props: ['project'],
+        data(){
+            return{
+                store,
+                titleMaxLen: 25,
+                bodyMaxLen: 100,
+            }
+        },
+        computed:{
+            truncateTitle(){
+                if(this.project.title.length > this.titleMaxLen){
+                    return this.project.title.substr(0,this.titleMaxLen) + '...';
+                }
+                return this.post.title;            
+            },
+            truncateBody(){
+                if(this.project.body.length > this.bodyMaxLen){
+                    return this.project.body.substr(0,this.bodyMaxLen) + '...';
+                }
+                return this.project.body;            
+            }
+        }
     }
 </script>
 
