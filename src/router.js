c423243188407556f8b2ad7306a78c8ace2b6286
Eecodev <mnasser.bbk@gmail.com>
2024-01-24 17:21:21 +0100

    AppContact added and imported to router

--- src/router.js
+++ src/router.js
@@ -2,7 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import AppHome from "./pages/AppHome.vue";
 import AppProjects from './pages/AppProjects.vue';
 import SingleProject from './pages/SingleProject.vue';
+import AppContact from './pages/AppContact.vue';
 import NotFound from './pages/NotFound.vue';
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,6 +23,11 @@ const router = createRouter({
             path: "/projects/:id",
             name: "single-project",
             component: SingleProject
+        },
+        {
+            path: "/contacts",
+            name: "contacts",
+            component: AppContact
         },
         {
             path: "/:pathMatch(.*)*",
